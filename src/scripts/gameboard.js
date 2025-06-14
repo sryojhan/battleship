e3d8b5b9ad87634a036981e05b341129e4d1379b
Yojhan <sryojhan@gmail.com>
2025-06-14 06:08:40 +0200

    Ship and battleship testing

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,3 +1,124 @@
-class Gameboard {}
+/*
+    Create a Gameboard class/factory.
+    Gameboards should be able to place ships at specific coordinates by calling the ship factory or class.
+    Gameboards should have a receiveAttack function that takes a pair of coordinates, determines whether or not the attack hit a ship and then sends the ‘hit’ function to the correct ship, or records the coordinates of the missed shot.
+    Gameboards should keep track of missed attacks so they can display them properly.
+    Gameboards should be able to report whether or not all of their ships have been sunk.
 
-export default Gameboard;
+    - Place ship
+    - ReceiveAttack
+    - Manage ship's health
+    - Manage missed shots
+    - Know if everything is sunk
+
+    typical size = 10x10
+    display positions: [1-10, A-J]
+
+    ships: 1 x Carrier (5), 2 x Battleship (4), 3 x Destroyer (3), 4 x Submarine (3), 5 x Patrol Boat (2) 
+*/
+
+import CreateShip from "./ship";
+
+const CreateGameboard = function (size) {
+
+    if (size < 0) throw new Error('Size error');
+
+    const grid = new Array(size);
+
+    for(let it = 0; it < size; it++){
+        grid[it] = new Array(size).fill(null);
+    }
+
+    const allShotPositions = [];
+    const ships = [];
+
+    const IsPointInsideBounds = function (position) {
+
+        if (
+            position[0] < 0 || position[1] < 0 ||
+            position[0] >= size || position[1] >= size
+        ) { return false; }
+        return true;
+    }
+
+    const IsPositionAvaliable = function (position) {
+
+        if (!IsPointInsideBounds(position)) return false;
+
+        return grid[position[0]][position[1]] === null;
+    }
+
+    /**
+     * 
+     * @param {*} topLeftCoordinate 
+     * @param {*} length 
+     * @param {*} isHorizontal
+     * @returns {boolean} Returs whether the ship was created correctly
+     */
+    const PlaceShip = function (topLeftCoordinate, length, isHorizontal) {
+
+        const increment = isHorizontal ? [1, 0] : [0, 1];
+
+        const CalculatePosition = function (i) {
+
+            return [topLeftCoordinate[0] + increment[0] * i, topLeftCoordinate[1] + increment[1] * i];
+        }
+
+        //* First check if all the positions are valid
+
+        for (let i = 0; i < length; i++) {
+
+            const position = CalculatePosition(i);
+
+            if (!IsPositionAvaliable(position)) return false;
+        }
+
+        const ship = CreateShip(length);
+
+        for (let i = 0; i < length; i++) {
+
+            const position = CalculatePosition(i);
+
+            grid[position[0]][position[1]] = ship;
+        }
+
+
+        ships.push(ship);
+
+        return true;
+    }
+
+    /**
+     * 
+     * @param {*} position 
+     * @returns -1 if the position is invalid, 0 if it missed and 1 if it hit a ship
+     */
+    const AttackPosition = function (position) {
+
+        if (!IsPointInsideBounds(position)) return -1;
+        if (allShotPositions.find((elem) => elem[0] === position[0] && elem[1] === position[1])) return -1;
+
+        allShotPositions.push(position);
+        const ship = grid[position[0]][position[1]];
+
+
+        if (ship) {
+
+            ship.Hit();
+            return 1;
+        }
+
+        return 0;
+    }
+
+    const IsEverythingSunk = function () {
+
+        if (ships.length === 0) return true;
+
+        return ships.every(ship => ship.IsSunk());
+    }
+
+    return { PlaceShip, AttackPosition, IsEverythingSunk };
+}
+
+export default CreateGameboard;
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,2 +1,197 @@
 
-import Gameboard from "../src/scripts/gameboard";
+import CreateGameboard from "../src/scripts/gameboard";
+
+
+test('Basic initialisation', ()=>{
+
+    CreateGameboard(10);
+});
+
+test('Basic initialisation', ()=>{
+
+    expect(()=>CreateGameboard(-1)).toThrow("Size error");
+});
+
+test('Place valid boat', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    expect(gameBoard.PlaceShip([2, 2], 3, true)).toBe(true);
+});
+
+test('Place invalid ship (left)', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+
+    expect(gameBoard.PlaceShip([-1, 2], 3, true)).toBe(false);
+});
+
+
+test('Place invalid ship (right)', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    expect(gameBoard.PlaceShip([9, 2], 3, true)).toBe(false);
+});
+
+
+test('Place invalid ship (top)', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+
+    expect(gameBoard.PlaceShip([2, -2], 3, false)).toBe(false);
+});
+
+
+test('Place invalid ship (bottom)', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    expect(gameBoard.PlaceShip([2, 7], 4, false)).toBe(false);
+});
+
+
+test('Overlaping ships', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    gameBoard.PlaceShip([2, 2], 2, false);
+
+    expect(gameBoard.PlaceShip([0, 2], 4, true)).toBe(false);
+});
+
+
+test('Adjacent ships', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    gameBoard.PlaceShip([4, 2], 2, false);
+    
+    expect(gameBoard.PlaceShip([0, 2], 4, true)).toBe(true);
+});
+
+test('Attack ship', ()=>{
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+
+    expect(gameBoard.AttackPosition([2, 2])).toBe(1);
+});
+
+test('Miss attack', ()=>{
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+
+    expect(gameBoard.AttackPosition([2, 3])).toBe(0);
+
+});
+
+test('Attack outside the board', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+
+    expect(gameBoard.AttackPosition([-1, 3])).toBe(-1);
+});
+
+test('Attack same spot', () =>{
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+
+    gameBoard.AttackPosition([2, 2]);
+    expect(gameBoard.AttackPosition([2, 2])).toBe(-1);
+
+});
+
+
+test('Everything sunk when empty', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    expect(gameBoard.IsEverythingSunk()).toBe(true);
+});
+
+test('Dont sink everything', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    gameBoard.PlaceShip([2, 2], 2, true);
+    gameBoard.AttackPosition([2, 2]);
+
+    const value = gameBoard.IsEverythingSunk();
+
+    expect(value).toBe(false);
+});
+
+
+test('Sink everything', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    gameBoard.PlaceShip([2, 2], 2, true);
+    
+    gameBoard.AttackPosition([2, 2]);
+    gameBoard.AttackPosition([3, 2]);
+    
+    expect(gameBoard.IsEverythingSunk()).toBe(true);
+});
+
+test('Sink everything (vertical ship)', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 4, false);
+
+    gameBoard.AttackPosition([2, 2]);
+    gameBoard.AttackPosition([2, 3]);
+    gameBoard.AttackPosition([2, 4]);
+    gameBoard.AttackPosition([2, 5]);
+
+    expect(gameBoard.IsEverythingSunk()).toBe(true);
+});
+
+
+test('Dont sink everything, multiple ships', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+    gameBoard.PlaceShip([2, 4], 2, true);
+
+    gameBoard.AttackPosition([2, 2]);
+    gameBoard.AttackPosition([3, 2]);
+
+    expect(gameBoard.IsEverythingSunk()).toBe(false);
+});
+
+test('Sink everything, multiple ships', ()=>{
+
+      const gameBoard = CreateGameboard(10);
+    gameBoard.PlaceShip([2, 2], 2, true);
+    gameBoard.PlaceShip([2, 4], 2, true);
+
+    gameBoard.AttackPosition([2, 2]);
+    gameBoard.AttackPosition([3, 2]);
+
+    gameBoard.AttackPosition([2, 4]);
+    gameBoard.AttackPosition([3, 4]);
+
+    expect(gameBoard.IsEverythingSunk()).toBe(true);
+});
+
+test('Miss after sinking everything', ()=>{
+
+    const gameBoard = CreateGameboard(10);
+
+    gameBoard.PlaceShip([2, 2], 2, true);
+    gameBoard.PlaceShip([2, 4], 2, true);
+
+    gameBoard.AttackPosition([2, 2]);
+    gameBoard.AttackPosition([3, 2]);
+
+    gameBoard.AttackPosition([2, 4]);
+    gameBoard.AttackPosition([3, 4]);
+
+    gameBoard.AttackPosition([0, 0]);
+
+    expect(gameBoard.IsEverythingSunk()).toBe(true);
+});
